2b3e2aa7041e52ebf2d3cc088664a9c81c27c6d0
PlGGS <bbor0422@gmail.com>
2016-10-30 23:33:34 -0500

    Add project files.

---
+++ GTA V Base Script/BaseScript.cs
@@ -0,0 +1,35 @@
+﻿using GTA;
+using System;
+using System.Windows.Forms;
+
+public class BaseScript : Script {
+
+    public BaseScript() {
+
+        Tick += OnTick;
+        KeyDown += OnKeyDown;
+        KeyUp += OnKeyUp;
+
+        Interval = 1;
+
+    }
+
+    void OnTick(object sender, EventArgs e) {
+
+
+
+    }
+
+    void OnKeyDown(object sender, KeyEventArgs e) {
+
+
+
+    }
+
+    void OnKeyUp(object sender, KeyEventArgs e) {
+
+
+
+    }
+
+}
